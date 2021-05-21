8fc64985399f7d31f221fd6c6623cbb10e0fba5d
Qianbo Yin <graysonyin717@gmail.com>
2021-05-20 20:02:20 -0700

    Initial Commit

---
+++ RL_Running_Man_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RL_Running_Man_Storyboard
+//
+//  Created by Qianbo Yin on 5/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
