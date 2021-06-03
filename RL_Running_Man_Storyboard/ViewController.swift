14c9dcccd9aa7773be9dd9fae4a86b6b0ac15d5d
Qianbo Yin <graysonyin717@gmail.com>
2021-06-02 19:17:49 -0700

    initial commit

--- RL_Running_Man_Storyboard/ViewController.swift
+++ RL_Running_Man_Storyboard/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func playVideo(_ sender: UIButton) {
+        // access to local video
+        guard let path = Bundle.main.path(forResource: "agent", ofType:"mp4") else {
+                debugPrint("video not found")
+                return
+            }
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
 
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
 }
 
